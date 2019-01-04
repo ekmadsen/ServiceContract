35f464062270e253a8bee957ff28e8f2cef68b64
Erik Madsen <ekmadsen@comcast.net>
2019-01-04 15:48:18 -0600

    Adding source code for Service Contract.

---
+++ Service Contract/CustomHttpHeader.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+
+namespace ErikTheCoder.ServiceContract
+{
+    [UsedImplicitly]
+    public static class CustomHttpHeader
+    {
+        [UsedImplicitly] public const string CorrelationId = "CorrelationId";
+    }
+}
