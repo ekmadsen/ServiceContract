61bcb60baa5390afda886a33259636ca2d65397d
Erik Madsen <ekmadsen@comcast.net>
2019-01-06 22:29:42 -0600

    Added EnvironmentName.

---
+++ Service Contract/EnvironmentName.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+
+namespace ErikTheCoder.ServiceContract
+{
+    [UsedImplicitly]
+    public static class EnvironmentName
+    {
+        [UsedImplicitly] public static string Dev = "Dev";
+        [UsedImplicitly] public static string Text = "Test";
+        [UsedImplicitly] public static string Prod = "Prod";
+    }
+}
