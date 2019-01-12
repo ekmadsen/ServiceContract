ff3251a35c3eaca2e930d4e1db19ff0f04e0cdcd
Erik Madsen <ekmadsen@comcast.net>
2019-01-12 15:39:55 -0600

    Added extension method that enables iterating over a Dictionary using tuple deconstruction.

---
+++ Service Contract/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+
+namespace ErikTheCoder.ServiceContract
+{
+    [UsedImplicitly]
+    public static class ExtensionMethods
+    {
+        [UsedImplicitly]
+        public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> Tuple, out T1 Key, out T2 Value)
+        {
+            Key = Tuple.Key;
+            Value = Tuple.Value;
+        }
+    }
+}
