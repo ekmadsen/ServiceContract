e2321bc9974f134c8ac9880b318d9c0ce8eea7f4
Erik Madsen <ekmadsen@comcast.net>
2019-05-23 11:57:49 -0500

    Added SafeRandom class.

---
+++ Service Contract/ISafeRandom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using JetBrains.Annotations;
+
+
+namespace ErikTheCoder.ServiceContract
+{
+    public interface ISafeRandom : IDisposable
+    {
+        [UsedImplicitly] int Next(int InclusiveMin, int ExclusiveMax);
+        [UsedImplicitly] void NextBytes(byte[] Bytes);
+    }
+}
