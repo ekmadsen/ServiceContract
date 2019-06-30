5fee34c69543f1f1af2617c8f75b622ae45a22db
Erik Madsen <ekmadsen@comcast.net>
2019-06-30 12:14:43 -0500

    Moved code to ErikTheCoder.Utilities solution and referenced its NuGet package.

--- Service Contract/CustomClaimType.cs
+++ Service Contract/CustomClaimType.cs
@@ -4,6 +4,7 @@
     {
         private const string _namespace = "https://schema.erikthecoder.net/claims/";
 
+
         // Can't use string interpolation to assign a constant.
         public const string FirstName = _namespace + "firstname";
         public const string LastName = _namespace + "lastname";
--- Service Contract/User.cs
+++ Service Contract/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Security.Claims;
+using ErikTheCoder.Utilities;
 using JetBrains.Annotations;
 
 
