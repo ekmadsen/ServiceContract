c222bc3cf4e1f40dfd17e7a30c08b1e72c8bd7be
Erik Madsen <ekmadsen@comcast.net>
2019-08-11 12:54:31 -0500

    Added User.ToAppUser extenion method.

---
+++ Service Contract/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using JetBrains.Annotations;
+
+
+namespace ErikTheCoder.ServiceContract
+{
+    [UsedImplicitly]
+    public static class ExtensionMethods
+    {
+        [UsedImplicitly]
+        public static User ToAppUser(this ClaimsPrincipal User) => ServiceContract.User.ParseClaims(User.Claims);
+    }
+}
